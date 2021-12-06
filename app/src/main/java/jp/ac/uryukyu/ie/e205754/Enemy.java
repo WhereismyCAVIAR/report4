908b7d809b99b83a4200446cc7fb06a723fe8934
Kakeru YAMAMORI <e205754@ie.u-ryukyu.ac.jp>
2021-12-06 09:40:57 +0900

    step1

--- app/src/main/java/jp/ac/uryukyu/ie/e205754/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205754/Enemy.java
@@ -8,11 +8,7 @@ package jp.ac.uryukyu.ie.e205754;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Enemy extends LivingThing{
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -20,25 +16,8 @@ public class Enemy {
      * @param maximumHP モンスターのHP
      * @param attack モンスターの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(!dead){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }    
+    public Enemy (String name, int maximumHP, int attack) { 
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -46,6 +25,7 @@ public class Enemy {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
@@ -53,70 +33,4 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
-    /**
-     * 自身の名前を返すgetter．
-     * 
-     * @return name :モンスター名
-     */
-    public String getName(){
-        return this.name;
-    }
-    /**
-     * 自身のHPを返すgetter
-     * 
-     * @return hitpoint :モンスターのHP
-     */
-    public int getHitPoint(){
-        return this.hitPoint;
-    }
-    /**
-     * 自身の攻撃力を返すgetter.
-     * 
-     * @return attack :モンスターの攻撃力
-     */
-    public int getAttack(){
-        return this.attack;
-    }
-    /**
-     * 自身の生死状態を返すgetter
-     * 
-     * @return 死亡している時 ture, 生存している時 false
-     */
-    public boolean getDead(){
-        return this.dead;
-    }
-    /**
-     * 自身の名前を設定するsetter.
-     * 
-     * @param _name 設定するモンスター名
-     */
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    /**
-     * 自身のHPを設定するsetter
-     * 
-     * @param _hitPoint 設定するモンスターのHP
-     */
-    public void setHitPoint(int hitPoint){
-        this.hitPoint = hitPoint;
-    }
-    /**
-     * 自身の攻撃力を設定するsetter
-     * 
-     * @param _attack 設定するモンスターの攻撃力
-     */
-    public void setAttack(int attack){
-        this.attack = attack;
-    }
-    /**
-     * 自身の生死状態を設定するsetter
-     * 
-     * @param _dead 設定するモンスターの生死状態
-     */
-    public void setDead(boolean dead){
-        this.dead = dead;
-    }
-
 }
